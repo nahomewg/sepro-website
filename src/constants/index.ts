b24d6568b7a72f7e7c0623f68403b5078f0e9362
Nahome Wolde-Giorgis <woldegio@ualberta.ca>
2024-07-08 21:55:50 -0600

    nav links constants

---
+++ src/constants/index.ts
@@ -0,0 +1,6 @@
+export const NAV_LINKS = [
+    { href: '/', key: 'training', label: 'Training'},
+    { href: '/', key: 'blog', label: 'Blog'},
+    { href: '/', key: 'shop', label: 'Shop'},
+    { href: '/', key: 'contact', label: 'Contact'},
+]
