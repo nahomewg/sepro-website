4e3627dafc587083b0800953ed4a7ca680248499
Nahome Wolde-Giorgis <woldegio@ualberta.ca>
2024-07-17 17:47:06 -0600

    custom footer and nav-bar

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,20 +3,36 @@ import Image from "next/image"
 
 const Footer = () => {
   return (
-    <footer className="absolute bottom-0 w-full min-h-[100px] bg-orange flex justify-between items-center py-10 px-32">
-        <div>
-            <Link href={"/"} className="gap-2.5">
-                <div className="flex items-center">
-                    <Image src="/se-logo.svg" alt="logo" width={50} height={50} className="w-auto h-auto"/>
-                    <div className="-space-y-2">
-                        <p className="text-lg text-white font-bold">SE PRO-GRAM</p>
-                        <p className="text-base text-white font-light">SE PRODUCTIONS INC.</p>
+    <footer className="flex items-center justify-center py-4 px-10 lg:py-10 lg:px-32 bg-orange">
+        <div className="flex w-full flex-col gap-14">
+            <div className="flex flex-col items-start gap-[10%] md:flex-row">
+                <Link href={"/"} className="gap-2.5">
+                    <div className="flex items-center">
+                        <Image src="/se-logo.svg" alt="logo" width={50} height={50} className="w-auto h-auto"/>
+                        <div className="-space-y-2">
+                            <p className="text-lg font-bold">SE PRO-GRAM</p>
+                            <p className="text-base font-light">SE PRODUCTIONS INC.</p>
+                        </div>
                     </div>
-                </div>
-                <p className="text-lg text-white font-light">Created by Nahome Wolde-Giorgis. Icons by Icons8.</p>
-            </Link>
+                    <p className="text-lg font-light">Created by Nahome Wolde-Giorgis. Icons by Icons8.</p>
+                </Link>
+            </div>
         </div>
     </footer>
+    // <footer className="absolute bottom-0 w-full bg-orange flex justify-between items-center py-5 px-10 lg:py-10 lg:px-32">
+    //     <div>
+    //         <Link href={"/"} className="gap-2.5">
+    //             <div className="flex items-center">
+    //                 <Image src="/se-logo.svg" alt="logo" width={50} height={50} className="w-auto h-auto"/>
+    //                 <div className="-space-y-2">
+    //                     <p className="text-lg font-bold">SE PRO-GRAM</p>
+    //                     <p className="text-base font-light">SE PRODUCTIONS INC.</p>
+    //                 </div>
+    //             </div>
+    //             <p className="text-lg font-light">Created by Nahome Wolde-Giorgis. Icons by Icons8.</p>
+    //         </Link>
+    //     </div>
+    // </footer>
   )
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,16 +4,16 @@ import Link from "next/link"
 
 const Navbar = () => {
   return (
-    <nav className="py-10 px-32">
+    <nav className="flex justify-center py-10 px-10 lg:block lg:px-32">
         <div className="flex justify-between items-center">
             <Link href={"/"} className="flex gap-2.5 items-center">
                 <Image src="/se-logo.svg" alt="logo" width={50} height={50} className="w-auto h-auto"/>
-                <p className="text-lg text-white font-bold">SE PRO-GRAM</p>
+                <p className="text-lg font-bold">SE PRO-GRAM</p>
             </Link>
             <ul className="hidden h-full gap-28 lg:flex">
                 {NAV_LINKS.map((link) => (
                     <Link href={link.href} key={link.key}
-                    className="text-lg font-light text-white cursor-pointer transition-all hover:font-bold">
+                    className="text-lg font-light cursor-pointer transition-all hover:font-bold">
                         {link.label}
                     </Link>
                 ))}
