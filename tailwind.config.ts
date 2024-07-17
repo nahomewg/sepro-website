e08f296edd83a545a560792b0a471788650d7b82
Nahome Wolde-Giorgis <woldegio@ualberta.ca>
2024-07-17 17:45:45 -0600

    added secondary white and hero-background tailwindcss

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,11 +9,10 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "hero-background": "url('/hero-background.png')",
       },
       colors: {
+        'secondary-white': '#E5E5E5',
         'orange': '#F28A12',
       }
     },
