77dcd02f74fbc7176030e6770e71ed4b486cdcce
Nahome Wolde-Giorgis <woldegio@ualberta.ca>
2024-07-18 13:59:25 -0600

    changed hero font sizes on smaller width

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,8 +6,8 @@ const Hero = () => {
       <div className="bg-hero-background h-full w-full bg-cover bg-center opacity-70 absolute" />
       <div className="flex items-center justify-center h-full lg:h-auto lg:absolute lg:top-24 lg:right-24">
         <div className="z-10">
-          <h1 className="text-6xl font-bold">SEPRO-GRAM</h1>
-          <p className="text-4xl font-bold">TRAINING</p>
+          <h1 className="text-5xl md:text-6xl font-bold">SEPRO-GRAM</h1>
+          <p className="text-center lg:text-start text-4xl font-bold">TRAINING</p>
         </div>
       </div>
       <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10">
