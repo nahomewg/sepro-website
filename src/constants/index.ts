07e86cac352a640754c62fc9b51a3c878e544da4
Nahome Wolde-Giorgis <woldegio@ualberta.ca>
2024-07-20 21:13:51 -0600

    dummy blog content

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,17 @@
 export const NAV_LINKS = [
-    { href: '/', key: 'training', label: 'Training'},
-    { href: '/', key: 'blog', label: 'Blog'},
-    { href: '/', key: 'shop', label: 'Shop'},
-    { href: '/', key: 'contact', label: 'Contact'},
+    { href: '/', id: 'training', label: 'Training'},
+    { href: '/', id: 'blog', label: 'Blog'},
+    { href: '/', id: 'shop', label: 'Shop'},
+    { href: '/', id: 'contact', label: 'Contact'},
+]
+
+export const DUMMY_BLOGS = [
+    { title: '10 tips for losing weight', id: '1', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.', image: '/ropes.png', date: '2022-01-01' },
+    { title: '10 tips for losing weight', id: '2', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.', image: '/ropes.png', date: '2022-01-01' },
+    { title: '10 tips for losing weight', id: '3', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.', image: '/ropes.png', date: '2022-01-01' },
+    { title: '10 tips for losing weight', id: '4', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.', image: '/ropes.png', date: '2022-01-01' },
+    { title: '10 tips for losing weight', id: '5', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.', image: '/ropes.png', date: '2022-01-01' },
+    { title: '10 tips for losing weight', id: '6',body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.', image: '/ropes.png', date: '2022-01-01' },
+    { title: '10 tips for losing weight', id: '7', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.', image: '/ropes.png', date: '2022-01-01' },
+    { title: '10 tips for losing weight', id: '8', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.', image: '/ropes.png', date: '2022-01-01' },
 ]
