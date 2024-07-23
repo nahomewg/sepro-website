30fe65dbdb1ebb1b84adb78807ef99305d78c1e5
Nahome Wolde-Giorgis <woldegio@ualberta.ca>
2024-07-22 18:57:27 -0600

    added hover property to main logo in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,9 +6,9 @@ const Navbar = () => {
   return (
     <nav className="flex justify-center py-10 px-10 lg:block lg:px-32">
         <div className="flex justify-between items-center">
-            <Link href={"/"} className="flex gap-2.5 items-center">
+            <Link href={"/"} className="flex gap-2.5 items-center group">
                 <Image src="/se-logo.svg" alt="logo" width={50} height={50} className="w-auto h-auto"/>
-                <p className="text-lg font-bold">SE PRO-GRAM</p>
+                <p className="text-lg font-bold group-hover:font-extrabold group-hover:drop-shadow-[2px_2px_2px_#f28a12] group-hover:transition-all">SE PRO-GRAM</p>
             </Link>
             <ul className="hidden h-full gap-28 lg:flex">
                 {NAV_LINKS.map((link) => (
