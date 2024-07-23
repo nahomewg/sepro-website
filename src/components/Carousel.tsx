349557dcc71e833b63a8732d3bbda4d48a294de1
Nahome Wolde-Giorgis <woldegio@ualberta.ca>
2024-07-22 18:56:23 -0600

    added rating icon component and global color

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -4,6 +4,7 @@ import useEmblaCarousel from 'embla-carousel-react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { Avatar } from '@nextui-org/avatar';
+import { Rating } from '@mui/material';
 
 type PropType = {
     slides: any[]
@@ -22,13 +23,13 @@ const Carousel: React.FC<PropType> = (props) => {
                     {slides.map((slide) => (
                         <div key={slide.id} className={`emblaSlide ${contentType == 'blog' ? 'h-48' : 'h-80'}`}>
                             {contentType == 'blog' &&
-                                <Link href={"/"} key={slide.id} className='flex relative items-center justify-center w-full h-full border-orange border-2 rounded-3xl'>
+                                <Link href={"/"} key={slide.id} className='flex relative items-center justify-center w-full h-full border-orange border-2 rounded-3xl transition-all hover:font-bold hover:border-3'>
                                     <Image src={slide.image} alt="Background image" width={0} height={0} sizes='100vw' style={{ objectFit: 'cover' }} className='w-full h-full absolute rounded-3xl opacity-50' />
                                     <p className='line-clamp-2 px-5 text-base sm:text-2xl md:text-3xl capitalize drop-shadow-[2px_2px_0_rgba(0,0,0,1)] font-semibold z-10'>{slide.title}</p>
                                 </Link>
                             }
                             {contentType == 'testimonial' &&
-                                <Link href={"/"} className='flex flex-col w-full h-full border-orange border-2 rounded-3xl'>
+                                <Link href={"/"} className='flex flex-col w-full h-full border-orange border-2 rounded-3xl transition-all hover:font-bold hover:border-3'>
                                     <div className='flex h-1/2 border-b-1 border-orange mx-5 py-5'>
                                         <p className='line-clamp-5 text-secondary-white text-base font-normal'>{slide.body}</p>
                                     </div>
@@ -36,11 +37,7 @@ const Carousel: React.FC<PropType> = (props) => {
                                         <Avatar name={slide.full_name[0]} color='warning' className='w-12 h-12 sm:w-16 sm:h-16 md:w-18 md:h-18 font-extrabold text-2xl sm:text-3xl md:text-4xl' />
                                         <div className='flex flex-col'>
                                             <p className='text-secondary-white text-base font-normal'>{slide.full_name}</p>
-                                            <div className='flex gap-1 pt-2'>
-                                                {[...Array(slide.rating)].map((_, index) => (
-                                                    <Image src={'/star.png'} alt="Gold star" width={20} height={20} key={index}></Image>
-                                                ))}
-                                            </div>
+                                            <Rating name='read-only' value={slide.rating} readOnly />
                                         </div>
                                     </div>
                                 </Link>
