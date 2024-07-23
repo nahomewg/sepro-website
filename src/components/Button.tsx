6d9518d86245d5c38a6097e8b14c67cc6c16f370
Nahome Wolde-Giorgis <woldegio@ualberta.ca>
2024-07-22 18:56:39 -0600

    added hover properties to button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -12,9 +12,9 @@ type ButtonProps = {
 
 const Button = ( { type, title, icon, color, textColor, borderSize, borderColor }: ButtonProps ) => {
   return (
-    <button className={`gap-3 rounded-3xl flex items-center px-10 py-5 ${color ? color : 'orange'} ${borderSize ? borderSize : '2'} ${borderColor ? borderColor : 'orange'}`} type={type}>
-    {icon && <Image src={icon} alt={title} width={24} height={24} />}
-    <label className={`text-xl font-semibold cursor-pointer ${textColor ? textColor : 'none'}` }>{title}</label>
+    <button className={`gap-3 rounded-full flex items-center px-10 py-5 hover:saturate-200 group ${color ? color : 'bg-orange'} ${borderSize ? borderSize : '2'} ${borderColor ? borderColor : 'orange'}`} type={type}>
+      {icon && <Image src={icon} alt={title} width={24} height={24} />}
+      <label className={`text-xl cursor-pointer group-hover:font-bold group-hover:transition-all ${textColor ? textColor : 'none'}` }>{title}</label>
     </button>
   )
 }
