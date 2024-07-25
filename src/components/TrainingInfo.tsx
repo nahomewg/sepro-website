ab73feffc878a7995a27082241c43b0061d422dc
Nahome Wolde-Giorgis <woldegio@ualberta.ca>
2024-07-25 17:46:49 -0600

    changed padding on trainingInfo

--- src/components/TrainingInfo.tsx
+++ src/components/TrainingInfo.tsx
@@ -3,9 +3,9 @@ import InfoCards from "./InfoCards"
 
 const TrainingInfo = () => {
   return (
-    <section className="px-12">
-        <h2 className="text-3xl md:text-4xl text-center font-bold">Training</h2>
-        <InfoCards trainingInfo={DUMMY_TRAINING_INFO} preview={false} />
+    <section className="px-10 lg:px-32">
+      <h2 className="text-3xl md:text-4xl text-center font-bold">Training</h2>
+      <InfoCards trainingInfo={DUMMY_TRAINING_INFO} preview={false} options={{ align: 'center', loop: true }} />
     </section>
   )
 }
