3926141124de090b115f67fd652782859739eca5
Nahome Wolde-Giorgis <woldegio@ualberta.ca>
2024-07-31 17:09:34 -0600

    added props to quiz components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,14 @@ import Hero from "@/components/Hero";
 import ProgramInfo from "@/components/ProgramInfo";
 import Quiz from "@/components/Quiz";
 import TestimonialGallery from "@/components/TestimonialGallery";
+import { DUMMY_QUESTIONS } from "@/constants";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <ProgramInfo />
-      <Quiz />
+      <Quiz questions={DUMMY_QUESTIONS} />
       <BlogGallery />
       <TestimonialGallery />
     </>
--- src/app/training/page.tsx
+++ src/app/training/page.tsx
@@ -1,9 +1,12 @@
+import Quiz from "@/components/Quiz";
 import TrainingInfo from "@/components/TrainingInfo";
+import { DUMMY_QUESTIONS } from "@/constants";
 
 export default function Training() {
     return (
         <>
             <TrainingInfo />
+            <Quiz questions={DUMMY_QUESTIONS} />
         </>
     )
 }
