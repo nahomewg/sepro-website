dd98c7037ece0e9b3773ed55a9dd0651515f6877
Nahome Wolde-Giorgis <woldegio@ualberta.ca>
2024-07-31 15:32:29 -0600

    set marks dynamically, updated question interface for DB

--- src/components/Questions.tsx
+++ src/components/Questions.tsx
@@ -1,5 +1,6 @@
 import { Radio, Slider } from "@mui/material";
-import { useState } from "react";
+import { Mark } from "@mui/material/Slider/useSlider.types";
+import { useEffect, useState } from "react";
 
 type PropType = {
     questions: Questions[];
@@ -9,6 +10,22 @@ type PropType = {
 const Questions: React.FC<PropType> = (props) => {
   const { questions, activeStep } = props;
   const [selectedValue, setSelectedValue] = useState('');
+  const [marks, setMarks] = useState<Mark[]>([]);
+  const [maxValue, setMaxValue] = useState(0);
+
+  useEffect(() => {
+    if (questions[activeStep]?.questionType === 'slider') {
+      const options = questions[activeStep].options || [];
+      const newMarks = options.map((option) => {
+        return {
+          value: options.indexOf(option),
+          label: option.option,
+        };
+      });
+      setMarks(newMarks);
+      setMaxValue(options.length - 1);
+    }
+  }, [questions, activeStep]);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSelectedValue(event.target.value);
@@ -22,8 +39,8 @@ const Questions: React.FC<PropType> = (props) => {
                 <div className="flex flex-col items-center gap-5 py-12 lg:justify-items-center lg:grid lg:grid-cols-2">
                     {questions[activeStep].options?.map((option, index) => (
                         <label key={index} className="flex items-center gap-6 p-6 border-2 border-orange cursor-pointer rounded-3xl max-w-md xl:max-w-xl w-full transition-all hover:font-bold hover:border-3">
-                            <Radio checked={selectedValue === option} value={option} onChange={handleChange} />
-                            <p className="text-xl">{option}</p>
+                            <Radio checked={selectedValue === option.option} value={option.option} onChange={handleChange} />
+                            <p className="text-xl">{option.option}</p>
                         </label>
                     ))}
                 </div>
@@ -40,7 +57,7 @@ const Questions: React.FC<PropType> = (props) => {
         {questions[activeStep].questionType == 'slider' &&
             <div>
                 <p className="text-xl capitalize text-center">{questions[activeStep].question}</p>
-                <Slider className="py-12" defaultValue={0} marks min={0} max={5} />
+                <Slider className="py-12" defaultValue={0} marks={marks} min={0} max={maxValue}  />
 
             </div>
         }
