7ef5e4a4571ccb66037f22c233f5b74432b4af52
Nahome Wolde-Giorgis <woldegio@ualberta.ca>
2024-08-02 15:04:20 -0600

    ui clean up on blog page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -8,9 +8,9 @@ export default function Blog() {
         <>
             <section className="w-full h-[660px] relative">
                 <div className="bg-blog-background h-full w-full bg-cover bg-center opacity-70 absolute" />
-                <Link href="#feature" className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">s
+                <Link href="#feature" className="absolute flex flex-col top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">s
                     <h1 className="text-5xl z-10">Blog</h1>
-                    <Image src="/Down.png" alt="Down arrow" className="w-full h-auto" width={0} height={0} sizes="100vw" />
+                    <Image src="/Down.png" alt="Down arrow" className="w-16 h-16 self-center" width={64} height={64} />
                 </Link>
             </section>
             <BlogCard id="feature" />
