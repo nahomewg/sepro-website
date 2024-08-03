2ed6a09419d1e749b0ea6506d0e0f8b7bd1ebf36
Nahome Wolde-Giorgis <woldegio@ualberta.ca>
2024-08-03 17:32:44 -0600

    fetch blogs and traininginfo in components

--- src/components/BlogGallery.tsx
+++ src/components/BlogGallery.tsx
@@ -1,6 +1,10 @@
-import { DUMMY_BLOGS } from "@/constants"
+'use client'
 import Carousel from "./Carousel"
 import { EmblaOptionsType } from "embla-carousel"
+import { useEffect, useState } from "react"
+import { getBlogs } from "@/actions/supaAction"
+import { Blog } from "@/app/interfaces/blog.interface"
+import { DUMMY_BLOGS } from "@/constants"
 
 const OPTIONS: EmblaOptionsType = {
     align: 'center',
@@ -9,6 +13,21 @@ const OPTIONS: EmblaOptionsType = {
 }
 
 const BlogGallery = () => {
+  // const [blogs, setBlogs] = useState<Blog[]>([]);
+
+  // useEffect(() => {
+  //   const fetchBlogs = async () => {
+  //     try {
+  //       const data = await getBlogs();
+  //       setBlogs(data);
+  //     } catch (error) {
+  //       console.error(error);
+  //     }
+  //   }
+
+  //   fetchBlogs();
+  // }, [])
+
   return (
     <div className="pb-48 px-10 lg:px-32 xl:px-64">
       <h2 className="text-3xl md:text-4xl pb-12 font-bold">Blog</h2>
--- src/components/ProgramInfo.tsx
+++ src/components/ProgramInfo.tsx
@@ -1,11 +1,28 @@
-import { DUMMY_TRAINING_INFO } from "@/constants"
+'use client'
 import InfoCards from "./InfoCards"
+import { useEffect, useState } from "react"
+import { getTrainingInfo } from "@/actions/supaAction";
+import { TrainingInfo } from "@/app/interfaces/training.interface";
 
 const ProgramInfo = () => {
+  const [trainingInfo, setTrainingInfo] = useState<TrainingInfo[]>([]);
+
+  useEffect(() => {
+    const fetchTrainingInfo = async () => {
+      try {
+        const data = await getTrainingInfo();
+        setTrainingInfo(data);
+      } catch (error) {
+        console.error(error);
+      }
+    } 
+
+      fetchTrainingInfo();
+    }, [])
   return (
     <section className="py-48 px-10 lg:px-32 xl:px-64">
       <h2 className="text-3xl md:text-4xl text-center font-bold">Are You Ready To Take Your Training To The Next Level?</h2>
-      <InfoCards trainingInfo={DUMMY_TRAINING_INFO} preview={true} />
+      <InfoCards trainingInfo={trainingInfo} preview={true} />
     </section>
   )
 }
