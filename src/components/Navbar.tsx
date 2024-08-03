a47cb2e355a214013415646bec4768ef9c506bc5
Nahome Wolde-Giorgis <woldegio@ualberta.ca>
2024-08-02 18:28:06 -0600

    updated navbar to use pathname

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,12 @@
-import { NAV_LINKS } from "@/constants"
-import Image from "next/image"
-import Link from "next/link"
+'use client'
+import { usePathname } from 'next/navigation';
+import { NAV_LINKS } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
 
 const Navbar = () => {
+  const pathname = usePathname();
+
   return (
     <nav className="flex justify-center py-10 px-10 lg:block lg:px-32">
         <div className="flex justify-between items-center">
@@ -12,15 +16,19 @@ const Navbar = () => {
             </Link>
             <ul className="hidden h-full gap-28 lg:flex">
                 {NAV_LINKS.map((link) => (
-                    <Link href={link.href} key={link.id}
-                    className="text-lg font-light cursor-pointer transition-all hover:font-bold">
-                        {link.label}
-                    </Link>
+                    <li key={link.id}>
+                        <Link 
+                            href={link.href} 
+                            className={`text-lg font-light cursor-pointer transition-all ${pathname === link.href ? 'font-bold' : 'hover:font-bold'}`}
+                        >
+                            {link.label}
+                        </Link>
+                    </li>
                 ))}
             </ul>
         </div>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
