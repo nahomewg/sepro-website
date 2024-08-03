8349a699f9abc874fcc95c88dac6fda4dc82cc87
Nahome Wolde-Giorgis <woldegio@ualberta.ca>
2024-08-02 18:28:34 -0600

    updated blog page to handle dynamic content loading

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,9 +1,27 @@
+'use client'
 import Image from "next/image"
 import Link from "next/link"
 import BlogCard from "@/components/BlogCard"
 import BlogGallery from "@/components/BlogGallery"
+import { useEffect, useState } from "react";
+import BlogContent from "@/components/BlogContent";
+import { useSearchParams } from "next/navigation"
+import { DUMMY_BLOGS } from "@/constants"
 
 export default function Blog() {
+    const [selectedBlog, setSelectedBlog] = useState<any>(null);
+    const searchParams = useSearchParams();
+    const blogId = searchParams.get("id");
+  
+    useEffect(() => {
+      if (blogId) {
+        const blog = DUMMY_BLOGS.find(blog => blog.id === blogId);
+        setSelectedBlog(blog);
+      } else {
+        setSelectedBlog(null);
+      }
+    }, [blogId]);
+  
     return (
         <>
             <section className="w-full h-[660px] relative">
@@ -13,7 +31,11 @@ export default function Blog() {
                     <Image src="/Down.png" alt="Down arrow" className="w-16 h-16 self-center" width={64} height={64} />
                 </Link>
             </section>
-            <BlogCard id="feature" />
+            {selectedBlog ? (
+                <BlogContent blog={selectedBlog} />
+            ) : (
+                <BlogCard id="feature" />
+            )}
             <BlogGallery />
         </>
     )
