5eb4a03586d5f5e2c6dc26237ab78ea49ce6301b
Nahome Wolde-Giorgis <woldegio@ualberta.ca>
2024-08-05 17:14:46 -0600

    abstracted fetching trainingInfo away from this component

--- src/components/TrainingInfo.tsx
+++ src/components/TrainingInfo.tsx
@@ -1,31 +1,8 @@
-'use client'
+import { ITrainingInfo } from "@/app/interfaces/training.interface"
 import InfoCards from "./InfoCards"
-import { useState, useEffect } from "react";
-import { getTrainingInfo } from "@/actions/supaAction";
-import { ITrainingInfo } from "@/app/interfaces/training.interface";
-import Loading from "@/app/loading";
 
-const TrainingInfo = () => {
-  const [trainingInfo, setTrainingInfo] = useState<ITrainingInfo[]>([]);
-  const [loading, setLoading] = useState(true);
-  
-  useEffect(() => {
-    const fetchTrainingInfo = async () => {
-      try {
-        const data = await getTrainingInfo();
-        setTrainingInfo(data);
-      } catch (error) {
-        console.error(error);
-      } finally {
-        setLoading(false);
-      }
-    } 
-      fetchTrainingInfo();
-  }, [])
+const TrainingInfo = ( { trainingInfo }: { trainingInfo: ITrainingInfo[] }) => {
 
-  if (loading) {
-    return <Loading />
-  }
   return (
     <section className="px-10 lg:px-32">
       <h2 className="text-3xl md:text-4xl text-center font-bold">Training</h2>
