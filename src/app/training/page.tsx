ca07754567c10fc1a525524aec5b3f7a1a46887c
Nahome Wolde-Giorgis <woldegio@ualberta.ca>
2024-08-05 17:13:51 -0600

    update training page to show selected training info

--- src/app/training/page.tsx
+++ src/app/training/page.tsx
@@ -1,12 +1,51 @@
-import Quiz from "@/components/Quiz";
+'use client'
+import TrainingContent from "@/components/TrainingContent";
 import TrainingInfo from "@/components/TrainingInfo";
-import { DUMMY_QUESTIONS } from "@/constants";
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import { ITrainingInfo } from "../interfaces/training.interface";
+import { getTrainingInfo } from "@/actions/supaAction";
+import Loading from "../loading";
 
 export default function Training() {
+    const [selectedTraining, setSelectedTraining] = useState<any>(null);
+    const searchParams = useSearchParams();
+    const trainingId = searchParams.get("id");
+    const [trainingInfo, setTrainingInfo] = useState<ITrainingInfo[]>([]);
+    const [loading, setLoading] = useState(true);
+    
+    useEffect(() => {
+      const fetchTrainingInfo = async () => {
+        try {
+          const data = await getTrainingInfo();
+          setTrainingInfo(data);
+        } catch (error) {
+          console.error(error);
+        } finally {
+          setLoading(false);
+        }
+      } 
+        fetchTrainingInfo();
+    }, [])
+  
+    useEffect(() => {
+        if (trainingId) {
+            const training = trainingInfo.find(training => training.id === trainingId);
+            setSelectedTraining(training);
+        } else {
+            setSelectedTraining(trainingInfo[0]);
+        }
+    }, [trainingId, trainingInfo]);
+
+    
+    if (loading) {
+      return <Loading />
+    }
+
     return (
         <>
-            <TrainingInfo />
-            <Quiz questions={DUMMY_QUESTIONS} />
+            <TrainingInfo trainingInfo={trainingInfo} />
+            {selectedTraining && <TrainingContent training={selectedTraining} />}
         </>
     )
 }
