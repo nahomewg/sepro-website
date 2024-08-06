97debfe3179e896f572fec817ad924f8a289478a
Nahome Wolde-Giorgis <woldegio@ualberta.ca>
2024-08-06 15:41:58 -0600

    fixed good routing for schema.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,7 +5,7 @@ config({ path: '.env' });
 
 export default defineConfig({
   dialect: "postgresql",
-  schema: "./db/src/schema.ts",
+  schema: "./src/db/schema.ts",
   out: "./supabase/migrations",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
