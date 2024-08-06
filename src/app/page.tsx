9ee6cc9afd8f8a74b91ce53b1038edf5f325faf7
Nahome Wolde-Giorgis <woldegio@ualberta.ca>
2024-08-06 15:42:33 -0600

    fetch questions + mapping options on fetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,36 @@
+'use client'
+import { getQuestions } from "@/actions/supaAction";
 import BlogGallery from "@/components/BlogGallery";
 import Hero from "@/components/Hero";
 import ProgramInfo from "@/components/ProgramInfo";
 import Quiz from "@/components/Quiz";
 import TestimonialGallery from "@/components/TestimonialGallery";
-import { DUMMY_QUESTIONS } from "@/constants";
+import { useEffect, useState } from "react";
+import Loading from "./loading";
 
 export default function Home() {
+  const [questions, setQuestions] = useState<Questions[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchQuestions = async () => {
+      try {
+        const data = await getQuestions();
+        setQuestions(data);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchQuestions();
+  }, [])
+
   return (
     <>
       <Hero />
       <ProgramInfo />
-      <Quiz questions={DUMMY_QUESTIONS} />
+      {loading ? <Loading /> : <Quiz questions={questions} />}
       <BlogGallery />
       <TestimonialGallery />
     </>
