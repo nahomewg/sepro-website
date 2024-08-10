9a10c1ef208fa27ac3db896a7f9d80b0e04ac5be
Nahome Wolde-Giorgis <woldegio@ualberta.ca>
2024-08-09 21:31:27 -0600

    stop fetching questions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,16 @@
-'use client'
-import { getQuestions } from "@/actions/supaAction";
 import BlogGallery from "@/components/BlogGallery";
 import Hero from "@/components/Hero";
 import ProgramInfo from "@/components/ProgramInfo";
 import Quiz from "@/components/Quiz";
 import TestimonialGallery from "@/components/TestimonialGallery";
-import { useEffect, useState } from "react";
-import Loading from "./loading";
+import { READINESS_QUIZ } from "@/constants";
 
 export default function Home() {
-  const [questions, setQuestions] = useState<Questions[]>([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchQuestions = async () => {
-      try {
-        const data = await getQuestions();
-        setQuestions(data);
-      } catch (error) {
-        console.error(error);
-      } finally {
-        setLoading(false);
-      }
-    }
-    fetchQuestions();
-  }, [])
-
   return (
     <>
       <Hero />
       <ProgramInfo />
-      {loading ? <Loading /> : <Quiz questions={questions} />}
+      <Quiz questions={READINESS_QUIZ} quizTitle="Readiness Quiz" />
       <BlogGallery />
       <TestimonialGallery />
     </>
