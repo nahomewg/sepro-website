4cdf898ecb545eb66562d063f9f3fc4708efdb85
Nahome Wolde-Giorgis <woldegio@ualberta.ca>
2024-08-16 17:20:02 -0600

    default test email for now

--- src/app/api/emails/route.ts
+++ src/app/api/emails/route.ts
@@ -5,11 +5,10 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(request: Request) {
     const body = await request.json();
-    const record = JSON.parse(body);
     try {
     const { data, error } = await resend.emails.send({
         from: 'SE PRO-GRAM <no-reply@woldecode.ca>',
-        to: [record['Email']],
+        to: ['nwgiorgis@gmail.com'],
         subject: 'SE PRO-GRAM - New Client!',
         react: QuizEmail({ body }),
     });
