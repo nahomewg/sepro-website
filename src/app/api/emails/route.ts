04d223b834092a44e9afb75e06042ba7cbee07d5
Nahome Wolde-Giorgis <woldegio@ualberta.ca>
2024-08-17 12:13:11 -0600

    dynamically change email type and subject line

--- src/app/api/emails/route.ts
+++ src/app/api/emails/route.ts
@@ -1,16 +1,32 @@
+import ContactEmail from '@/app/emails/ContactEmail';
 import QuizEmail from '@/app/emails/QuizEmail';
 import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(request: Request) {
-    const body = await request.json();
+    const { emailType, body } = await request.json();
+
+    let emailTemplate;
+    switch (emailType) {
+        case 'quiz':
+            emailTemplate = QuizEmail({ body });
+            break;
+        case 'contact':
+            emailTemplate = ContactEmail({ body });
+            break
+        default:
+            return Response.json({ error: 'Invalid email type' }, { status: 400 });
+    }
+
+    let subject = emailType === 'quiz' ? 'SE PRO-GRAM - New Quiz Submission!' : 'SE PRO-GRAM - New Client!';
+
     try {
     const { data, error } = await resend.emails.send({
         from: 'SE PRO-GRAM <no-reply@woldecode.ca>',
         to: ['nwgiorgis@gmail.com'],
-        subject: 'SE PRO-GRAM - New Client!',
-        react: QuizEmail({ body }),
+        subject: subject,
+        react: emailTemplate,
     });
 
     if (error) {
