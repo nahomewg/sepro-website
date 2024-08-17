c70928aba0bc890c12afead6b2878c2c47009600
Nahome Wolde-Giorgis <woldegio@ualberta.ca>
2024-08-17 12:12:25 -0600

    added outlined SE to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,7 +12,7 @@ const Navbar = () => {
         <div className="flex justify-between items-center">
             <Link href={"/"} className="flex gap-2.5 items-center group">
                 <Image src="/se-logo.svg" alt="logo" width={50} height={50} className="w-auto h-auto"/>
-                <p className="text-lg font-bold group-hover:font-extrabold group-hover:drop-shadow-[2px_2px_2px_#f28a12] group-hover:transition-all">SE PRO-GRAM</p>
+                <p className="text-lg font-bold group-hover:font-extrabold group-hover:drop-shadow-[2px_2px_2px_#f28a12] group-hover:transition-all"><span className="stroke-custom small-stroke">SE</span> PRO-GRAM</p>
             </Link>
             <ul className="hidden h-full gap-28 lg:flex">
                 {NAV_LINKS.map((link) => (
