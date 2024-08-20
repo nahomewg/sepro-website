d0f010eccc9f9dfb32cfe889553c4bec58c3a706
Nahome Wolde-Giorgis <woldegio@ualberta.ca>
2024-08-20 13:34:39 -0600

    delete date from training content

--- src/components/TrainingContent.tsx
+++ src/components/TrainingContent.tsx
@@ -8,7 +8,6 @@ interface TrainingContentProps {
 const TrainingContent: React.FC<TrainingContentProps> = ({ training }) => {
   return (
     <section className="pb-24 pt-10 px-10 lg:px-32 xl:px-64">
-      <p className="text-xl font-semibold">{formatDate(training.created_at)}</p>
       <h2 className="text-3xl pt-2 font-semibold text-left md:text-center uppercase">{training.title}</h2>
       <p className="text-xl pt-12 pb-24">{training.description}</p>
     </section>
