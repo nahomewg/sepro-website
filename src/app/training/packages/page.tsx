fff64150f2dde9e8826fd50ff2c9905c31f18803
Nahome Wolde-Giorgis <woldegio@ualberta.ca>
2024-09-01 15:06:31 -0600

    added new pages for the corresponding navlinks

---
+++ src/app/training/packages/page.tsx
@@ -0,0 +1,9 @@
+export default function Packages() {
+    return (
+        <>
+            <section className="w-full h-[660px] relative">
+                <h1>Packages</h1>
+            </section>
+        </>
+    )
+}
