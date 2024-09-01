e5d016fa9c6a5fab770110c88b546ec33b5d44da
Nahome Wolde-Giorgis <woldegio@ualberta.ca>
2024-09-01 15:08:03 -0600

    indenting

--- src/components/NavLinks.tsx
+++ src/components/NavLinks.tsx
@@ -19,38 +19,38 @@ const NavLinks = () => {
 
     return (
     <ul className="hidden h-full gap-28 items-center lg:flex">
-    {NAV_LINKS.map((link) => (
-        <li key={link.id}>
-            {link.children ?
-                <Dropdown isOpen={!!openDropdowns[link.id]} onMouseLeave={() => handleMouseLeave(link.id)} className="bg-black">
-                    <DropdownTrigger>
-                        <p
-                            onMouseEnter={() => {
-                                handleMouseEnter(link.id);
-                                }}
-                            className={`flex gap-2 text-base font-light cursor-pointer uppercase transition-all ${pathname === link.href ? 'font-bold' : 'hover:font-bold'}`}>
-                            {link.label}
-                            <Image src={"/chevron-down.png"} width={20} height={10} alt="down arrow"/>
-                        </p>
-                    </DropdownTrigger>
-                    <DropdownMenu color="primary">
-                        {link.children.map((child) => (
-                            <DropdownItem key={child.id} href={child.href} className="text-base uppercase font-light">
-                                {child.label}
-                            </DropdownItem>
-                        ))}
-                    </DropdownMenu>
-                </Dropdown>
-            : 
-                <Link 
-                    href={link.href} 
-                    className={`text-base font-light cursor-pointer uppercase transition-all ${pathname === link.href ? 'font-bold' : 'hover:font-bold'}`}
-                >
-                    {link.label}
-                </Link>
-            }
-        </li>
-    ))}
+        {NAV_LINKS.map((link) => (
+            <li key={link.id}>
+                {link.children ?
+                    <Dropdown isOpen={!!openDropdowns[link.id]} onMouseLeave={() => handleMouseLeave(link.id)} className="bg-black">
+                        <DropdownTrigger>
+                            <p
+                                onMouseEnter={() => {
+                                    handleMouseEnter(link.id);
+                                    }}
+                                className={`flex gap-2 text-base font-light cursor-pointer uppercase transition-all ${pathname === link.href ? 'font-bold' : 'hover:font-bold'}`}>
+                                {link.label}
+                                <Image src={"/chevron-down.png"} width={20} height={10} alt="down arrow"/>
+                            </p>
+                        </DropdownTrigger>
+                        <DropdownMenu color="primary">
+                            {link.children.map((child) => (
+                                <DropdownItem key={child.id} href={child.href} className="text-base uppercase font-light">
+                                    {child.label}
+                                </DropdownItem>
+                            ))}
+                        </DropdownMenu>
+                    </Dropdown>
+                : 
+                    <Link 
+                        href={link.href} 
+                        className={`text-base font-light cursor-pointer uppercase transition-all ${pathname === link.href ? 'font-bold' : 'hover:font-bold'}`}
+                    >
+                        {link.label}
+                    </Link>
+                }
+            </li>
+        ))}
     </ul>
     )
 }
